ff3054553fbe4c63a1661601555b27d56437e7ff
mulweli-mp <mulwelimushiana@yahoo.com>
2024-08-01 14:49:29 +0200

    Removed WalletConnect Experiment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { useState } from "react";
 import styles from "./page.module.css";
-import WalletConnectComponent from "@/components/WalletConnectComponent";
 
 export default function Home() {
   const [isWalletConnected, setIsWalletConnected] = useState(false);
@@ -9,7 +8,6 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <h1>NFT Marketplace</h1>
-      <WalletConnectComponent setWalletConnected={setIsWalletConnected} />
     </main>
   );
 }
