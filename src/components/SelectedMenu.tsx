412d8764c8c057732334f7cfeaf31790ced1e8df
mulweli-mp <mulwelimushiana@yahoo.com>
2024-08-02 05:16:07 +0200

    Added SelectedMenu component

---
+++ src/components/SelectedMenu.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./SelectedMenu.module.css";
+
+const SelectedMenu: React.FC = () => {
+  return <div className={styles.container}></div>;
+};
+
+export default SelectedMenu;
