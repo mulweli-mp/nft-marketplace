9f7a26ed15cb1c2b59ab8e47a2f51725c6f2a748
mulweli-mp <mulwelimushiana@yahoo.com>
2024-08-02 05:16:50 +0200

    Added components: Header, LeftMenu, BurgerMenu & SelectedMenu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,27 @@
 "use client";
-import { useState } from "react";
 import styles from "./page.module.css";
+import { useState } from "react";
+import LeftMenu from "@/components/LeftMenu";
+import BurgerMenu from "@/components/BurgerMenu";
+import SelectedMenu from "@/components/SelectedMenu";
+import Header from "@/components/Header";
 
 export default function Home() {
-  const [isWalletConnected, setIsWalletConnected] = useState(false);
+  const [isMobileMenOpen, setIsMobileMenOpen] = useState(false);
+
+  const toogleMobileMenu = (): void => {
+    setIsMobileMenOpen((prev) => !prev);
+  };
 
   return (
     <main className={styles.main}>
-      <h1>NFT Marketplace</h1>
+      <Header
+        isMobileMenOpen={isMobileMenOpen}
+        toogleMobileMenu={toogleMobileMenu}
+      />
+      <LeftMenu />
+      <BurgerMenu />
+      <SelectedMenu />
     </main>
   );
 }
