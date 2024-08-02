e8f7080680415ab2fd6abbaa25f7f6be219a613a
mulweli-mp <mulwelimushiana@yahoo.com>
2024-08-02 05:14:09 +0200

    Cart context setup

---
+++ src/context/CartContext.tsx
@@ -0,0 +1,45 @@
+"use client";
+import React, { createContext, useReducer, useContext, ReactNode } from "react";
+
+import { CartAction, CartContextType, CartState } from "@/types";
+
+const CartContext = createContext<CartContextType | null>(null);
+
+const cartReducer = (state: CartState, action: CartAction): CartState => {
+  switch (action.type) {
+    case "ADD_ITEM":
+      return {
+        ...state,
+        items: [...state.items, action.payload],
+      };
+    case "REMOVE_ITEM":
+      return {
+        ...state,
+        items: state.items.filter((item) => item.id !== action.payload.id),
+      };
+    default:
+      return state;
+  }
+};
+
+interface CartProviderProps {
+  children: ReactNode;
+}
+
+export const CartProvider: React.FC<CartProviderProps> = ({ children }) => {
+  const [state, dispatch] = useReducer(cartReducer, { items: [] });
+
+  return (
+    <CartContext.Provider value={{ state, dispatch }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export const useCart = (): CartContextType => {
+  const context = useContext(CartContext);
+  if (!context) {
+    throw new Error("useCart must be used within a CartProvider");
+  }
+  return context;
+};
